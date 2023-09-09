adb36e723df2eb2aa0cb84e58a3cfb07ec74a8d0
Débora Bastos <deborabastos96@gmail.com>
2023-09-09 06:07:15 -0300

    Almost finished

--- script.js
+++ script.js
@@ -6,26 +6,37 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-////////////////////////////////////////////////////////////////////////////////
-// Arrastar suave
-const allLinks = document.querySelectorAll("a:link");
+/////////////////////////////////////////////
+// Make mobile navigation work
 
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+const headerLink = document.querySelector(".main-nav-list");
 
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-  });
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
+
+headerLink.addEventListener("click", function () {
+  headerEl.classList.remove("nav-open");
+});
+
+//////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) document.body.classList.add("sticky");
+    if (ent.isIntersecting) document.body.classList.remove("sticky");
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
